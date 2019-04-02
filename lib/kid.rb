6ad8f81c843d83ddb786281f43458fc69721b124
Malcolm Leake <echelon1089@gmail.com>
2019-04-02 21:39:36 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,9 +1,8 @@
 require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 class Kid
- extend   FancyDance::ClassMethods
-
-  include 
+ extend FancyDance::ClassMethods
+ include 
   FancyDance::InstanceMethods
   
   attr_accessor :name 
